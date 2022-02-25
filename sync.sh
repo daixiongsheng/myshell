59c7396418acd3e48a38e216941118f954d604d2
daixiongsheng <daixiongsheng@gmail.com>
2022-02-25 21:48:09 +0800

    2022-02-25-21:48:09

---
+++ sync.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd $HOME/myshell
+
+if [[ "" != `git status --short` ]]
+then
+	git pull
+	git config user.email daixiongsheng@gmail.com
+	git add .
+	date=$(date "+%Y-%m-%d-%H:%M:%S")
+	echo $date $1
+	git commit -m "$date $1"
+	git push origin master
+	push
+fi
