7b990e008378d1949ef87d2cc462d3c334a0028c
daixiongsheng <daixiongsheng@gmail.com>
2022-04-11 19:01:01 +0800

    2022-04-11-19:01:01

--- sync.sh
+++ sync.sh
@@ -4,11 +4,11 @@ cd $HOME/myshell
 
 if [[ "" != `git status --short` ]]
 then
-	git pull
 	git config user.email daixiongsheng@gmail.com
 	git add .
 	date=$(date "+%Y-%m-%d-%H:%M:%S")
 	echo $date $1
 	git commit -m "$date $1"
+	git pull
 	git push origin master
 fi
